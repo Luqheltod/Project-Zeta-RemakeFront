c10573e94e1a5a1d60444410a5b49eadd402abde
45257012 <luqh555@gmail.com>
2024-03-04 02:53:59 +0100

    energy y vida issues where to put them ?

--- src/app/components/game-options/game-options.component.ts
+++ src/app/components/game-options/game-options.component.ts
@@ -1,16 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { Option } from '../../models/option';
 import { Stage } from '../../models/stage';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-game-options',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './game-options.component.html',
   styleUrl: './game-options.component.scss'
 })
 export class GameOptionsComponent {
 
+
+  health:Number=100;
+  energy:Number=100;
 
   @Input() stage! : Stage;
   buttonTestMsg = "";
