f342a499df8c428468406ec0cb9f9a01783fcd4e
45257012 <luqh555@gmail.com>
2024-03-31 19:43:35 +0200

    preparado un algoritmo para que descanse solo una vez por piso, por ultimo queda estilos de css

--- src/app/components/game-energylife/game-energylife.component.ts
+++ src/app/components/game-energylife/game-energylife.component.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, inject } from '@angular/core';
+import { Component, EventEmitter, Input, Output, SimpleChanges, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import { GameService } from '../../service/game.service';
 import { StageRepository } from '../../store/stage.repository';
 import { Stage, createStage } from '../../models/stage';
+import { RestInfo } from '../../models/restInfo';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SnackBarRestComponent } from '../../shared/snack-bar-rest/snack-bar-rest.component';
 @Component({
   selector: 'app-game-energylife',
   standalone: true,
@@ -16,14 +19,18 @@ export class GameEnergylifeComponent {
 
   @Input() health!:number;
   @Input() energy!:number;
-
+  @Input() flat!:string;
+  @Output() healthModifier: EventEmitter<number> = new EventEmitter<number>();
+  @Output() energyModifier: EventEmitter<number> = new EventEmitter<number>();
 
   disabled:boolean = false;
+  flatOfRest : string = "";
 
   stage!: Stage;
+  restInfo! : RestInfo | undefined;
   private readonly gameService = inject(GameService);
  
-  constructor( public stageRepository: StageRepository) {
+  constructor( public stageRepository: StageRepository,private readonly snackBar: MatSnackBar,  ) {
     
     this.stageRepository.getActualStage().subscribe( stage => {
       this.stage = stage;
@@ -31,15 +38,22 @@ export class GameEnergylifeComponent {
     
   }
 
-  rest(){
-    if(this.stage?.restModifier > 5){
-      this.disabled = true;
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['flat']){
+      if(this.allowedToRest(this.flat)){
+        this.disabled = false;
+      }
+      
     }
+  }
+
+  rest(){
     if(this.disabled){
-      this.disabled = true;
       return;
     }
-/*
+
+    this.flatOfRest = this.flat;
+
     this.gameService.rest(this.stage.restModifier).subscribe(restInfo => {
       this.restInfo = restInfo
 
@@ -61,6 +75,23 @@ export class GameEnergylifeComponent {
       this.disabled = true;
     } );
     
-*/
+
 }
+
+/** Si es un piso distinto al cual descansó, permitimos descansar */
+ allowedToRest(flat:string) : boolean {
+   // Utilizamos match para buscar la primera secuencia de caracteres que no sean números
+   const match = flat.match(/^[^\d]+/);
+  // Utilizamos una expresión regular para extraer el texto hasta el primer número
+   const actualFlat = match ? match[0].trim() : "";
+   const esVestibulo = actualFlat.toLowerCase() === 'vestibulo';
+
+   if(this.flatOfRest !== actualFlat  || esVestibulo ){
+    return true;
+   } else{
+    return false;
+   }
+
+}
+
 }
--- src/app/shared/snack-bar-rest/snack-bar-rest.component.ts
+++ src/app/shared/snack-bar-rest/snack-bar-rest.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MAT_SNACK_BAR_DATA, MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-snack-bar-rest',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './snack-bar-rest.component.html',
   styleUrl: './snack-bar-rest.component.scss'
 })
 export class SnackBarRestComponent {
 
+  constructor(@Inject(MAT_SNACK_BAR_DATA) public data: any,
+  public snackBar: MatSnackBar) { }
 }
